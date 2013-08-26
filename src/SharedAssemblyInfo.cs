c1a0931e021155552c051f413ac06db4f52a2a0e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-08-26 23:45:34 +0200

    Added SharedAssemblyInfo

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Milo")]
+[assembly: AssemblyDescription("Content Management System in .NET")]
+[assembly: AssemblyCompany("Milo")]
+[assembly: AssemblyProduct("Milo")]
+[assembly: AssemblyCopyright("Copyright (C) Fredirk Forsmo, Caroline Millgårdh and contributors")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+
