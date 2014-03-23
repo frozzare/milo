72b0512b46c4651c41bdfbc8049724e4961c2a39
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-03-23 13:35:58 +0100

    First commit

--- src/markup/config.rb
+++
@@ -1,4 +0,0 @@
-sass_dir = 'src/scss'
-css_dir = 'public/css'
-add_import_path 'bower_components/normalize-scss/'
-line_comments = false
--- src/markup/public/js/maria.js
+++
@@ -1,4 +0,0 @@
-// maria v0.0.1 | 2014-01-29
-(function () {
-  
-}).call(this);
